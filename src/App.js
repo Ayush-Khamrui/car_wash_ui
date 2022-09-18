d78dd989e614ff7e910b2f00b2eef5aad93d0611
Ayush-Khamrui <ayushkhamrui@outlook.com>
2022-09-19 00:21:18 +0530

    Added UI version 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { Home } from './views/Home';
+import { LoginCustomer } from './components/LoginCustomer';
+import { Navbar } from './components/Navbar';
+import { Register } from './components/Register';
+import SearchTable from './components/SearchTable';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { CustomerDashboard } from './views/CustomerDashboard';
+import { AdminDashboard } from './views/AdminDashboard';
+import { AdminLogin } from './views/AdminLogin';
+import { WasherLogin } from './views/WasherLogin';
+import { Order } from './components/Order';
+import { CustomerUpdate } from './components/CustomerUpdate';
+import Content from './views/blog/Content';
+import { NewsContextProvider } from './views/blog/NewsContext';
+import { AboutUs } from './views/AboutUs';
+import { WasherDashboard } from './views/WasherDashboard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/adminlogin" element={<AdminLogin/>} />
+          <Route path="/washerlogin" element={<WasherLogin/>} />
+          <Route path="/profile" element={<CustomerDashboard/>} />
+          <Route path="/adminprofile" element={<AdminDashboard/>} />
+          <Route path="/customersearch" element={<SearchTable/>} />
+          <Route path="/updatecustomer" element={<CustomerUpdate/>} />
+          <Route path="/order" element={<Order/>} />
+          <Route path="/blogs" element={<NewsContextProvider><Content/></NewsContextProvider>}/>
+          <Route path="/about" element={<AboutUs/>} />
+          <Route path="/washer" element={<WasherDashboard/>} />
+        </Routes>
+      </BrowserRouter>
+
+      
     </div>
   );
 }
